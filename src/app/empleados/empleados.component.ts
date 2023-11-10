fa6a12a77ea0128b99a0a7bed264d346df3b8695
Diego Palacios <palaciosdiegovalentino@gmail.com>
2023-11-10 06:22:08 -0300

    TRYING: Components Logic and flux

--- src/app/empleados/empleados.component.ts
+++ src/app/empleados/empleados.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EmpleadoComponent } from '../empleado/empleado.component';
 
 @Component({
   selector: 'app-empleados',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, EmpleadoComponent],
   templateUrl: './empleados.component.html',
   styleUrl: './empleados.component.css'
 })
