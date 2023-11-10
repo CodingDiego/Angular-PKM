fbb51a73713e4987b047b5e65b73976dd51dae28
Diego Palacios <palaciosdiegovalentino@gmail.com>
2023-11-10 05:59:19 -0300

    NEW: Empleado and Empleados' folders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { EmpleadosComponent } from './empleados/empleados.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, EmpleadosComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'CCAngular';
+  saludo = 'Hola manga de tarados';
 }
