0236e12c14f03f0a54d13371c116ad5b4e9412e9
Diego Palacios <palaciosdiegovalentino@gmail.com>
2023-11-11 07:15:56 -0300

    No message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,59 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CCAngular';
-  saludo = 'Hola manga de tarados';
+  displayValue: string|number = 0;
+  firstOperand: number|null = null;
+  operator: string|null = null;
+
+  appendNumber(value: number) {
+    this.displayValue = this.displayValue.toString() + value.toString();
+  }
+
+  setOperation(operator: string) {
+    this.firstOperand = Number(this.displayValue);
+    this.operator = operator;
+    this.displayValue = '';
+  }
+
+  calculate() {
+    if (this.firstOperand !== null && this.operator !== null) {
+      const secondOperand = Number(this.displayValue);
+      switch (this.operator) {
+        case '+':
+          this.displayValue = (this.firstOperand + secondOperand).toString();
+          break;
+        case '-':
+          this.displayValue = (this.firstOperand - secondOperand).toString();
+          break;
+        case '*':
+          this.displayValue = (this.firstOperand * secondOperand).toString();
+          break;
+        case '/':
+          if (secondOperand != 0) {
+            this.displayValue = (this.firstOperand / secondOperand).toString();
+          } else {
+            this.displayValue = "Error";
+          }
+          break;
+        default:
+          this.displayValue = "Error";
+      }
+      this.firstOperand = null;
+      this.operator = null;
+    }
+  }
+  clear() {
+    this.displayValue = '';
+    this.firstOperand = null;
+    this.operator = null;
+  }
 }
